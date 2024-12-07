0fddb5bc30c6e10c5005ac2fb86e59d1ff059620
vineet <vineetchaurasia2010@gmail.com>
2024-12-07 14:34:29 +0530

    first commit

---
+++ src/main/java/com/technicalshool/techincalschool/TechincalschoolApplication.java
@@ -0,0 +1,13 @@
+package com.technicalshool.techincalschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechincalschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechincalschoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technicalshool/techincalschool/TechincalschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technicalshool.techincalschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechincalschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
